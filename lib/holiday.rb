7b74021918b62d7527749fc89349e25eac798b95
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-05 14:05:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
       # new_holiday_name.collect do |word| #capitalizes each word 
       #   word.capitalize
       # end
-      print "\t#{(holiday.to_s.split("_").each {|w| w.capitalize binding.pry}).join(" ")}: #{things.join(", ")}\n"
+      print "\t#{(holiday.to_s.split("_").each {|w| w = w.capitalize}).join(" ")}: #{things.join(", ")}\n"
     end
   end
 end
